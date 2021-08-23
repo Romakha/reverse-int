2121394833bde3dbc73edb4d10327becf68a58a4
Romakha <55205695+Romakha@users.noreply.github.com>
2021-08-23 19:59:12 +0600

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  let result = ''
   
+  n = n + ''
+  for(let i = n.length-1; i >= 0; i--){
+    result+=n[i]
+  }
+  let a = parseInt(result)
+  return a
+
 }
